bee0d5e5229333503c087ce63f47dff9cd08989e
PiWter <alfrevr05elpro@gmail.com>
2024-12-15 20:53:07 +0100

    frames preload

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,50 @@ use std::time::Duration;
 use image::*;
 
 fn main() {
-    bad_apple(4);
+    preload_images();
 }
 
+fn preload_images(){
+    let mut frames: Vec<DynamicImage> = Vec::new();
 
-fn bad_apple(downscaling: u32){
-    for f in 100..=6562 {
+    for f in 100..=6562{
+        println!("Loading frame {} / 6562", f);
         let mut frame_path: String = String::from("image_sequence/bad_apple_");
         let file_number = f.to_string();
         for n in file_number.chars() {
             frame_path.push(n);
         }
         frame_path += ".png";
-        //println!("{frame_path}");
 
-        let img = image::open(&frame_path).unwrap();
-        let image_x_pixels = img.width();
-        let image_y_pixels = img.height();
+        frames.push(image::open(&frame_path).unwrap());
+    }
+    let image_x_pixels = frames[0].width();
+    let image_y_pixels = frames[0].height();
+    bad_apple(9, image_x_pixels, image_y_pixels, frames);
+}
 
+fn bad_apple(downscaling: u32, image_x_pixels: u32, image_y_pixels: u32, frames: Vec<DynamicImage>){
+    let mut frame: String;
+    for f in frames {
+        frame = "".to_string();
         for y in 1..image_y_pixels {
             for x in 1..image_x_pixels {
                 if y % (downscaling * 2) == 0 && x % (downscaling * 2) == 0{
-                    if img.get_pixel(x, y) == Rgba([0, 0, 0, 255]){
-                        print!("  ");
+                    if f.get_pixel(x, y) == Rgba([0, 0, 0, 255]){
+                        //print!("  ");
+                        frame += "  ";
                     } else {
-                        print!("+ ");
+                        //print!("+ ");
+                        frame += "+ ";
                     }
                 }
             }
             if y % (downscaling * 2) == 0{
-                println!("");
+                //println!("");
+                frame += "\n";
             }
         }
-        println!();
-        thread::sleep(Duration::from_millis(50));
+        println!("{frame}");
+        thread::sleep(Duration::from_millis(5));
     }
 }
