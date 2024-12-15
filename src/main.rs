ed5fb012601e222e7d75802ef0fc89f1cbfd345e
PiWter <alfrevr05elpro@gmail.com>
2024-12-15 19:29:21 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::time::Duration;
 use image::*;
 
 fn main() {
+    bad_apple(4);
+}
+
+
+fn bad_apple(downscaling: u32){
     for f in 100..=6562 {
         let mut frame_path: String = String::from("image_sequence/bad_apple_");
         let file_number = f.to_string();
@@ -18,15 +23,15 @@ fn main() {
 
         for y in 1..image_y_pixels {
             for x in 1..image_x_pixels {
-                if y % 20 == 0 && x % 20 == 0{
+                if y % (downscaling * 2) == 0 && x % (downscaling * 2) == 0{
                     if img.get_pixel(x, y) == Rgba([0, 0, 0, 255]){
-                        print!("   ");
+                        print!("  ");
                     } else {
-                        print!(" + ");
+                        print!("+ ");
                     }
                 }
             }
-            if y % 20 == 0{
+            if y % (downscaling * 2) == 0{
                 println!("");
             }
         }
