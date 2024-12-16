2463ee7367a961d20f4523996164f97483195326
PiWter <alfrevr05elpro@gmail.com>
2024-12-16 13:45:38 +0100

    faster video

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use image::*;
+use std::io;
 
 fn main() {
     preload_images();
@@ -11,12 +12,14 @@ fn main() {
     image_x_pixels: u32,
     image_y_pixels: u32
 }*/
+
 fn preload_images(){
     let mut frames: Vec<DynamicImage> = Vec::new();
 
     for f in 1..=6562{
         println!("Loading frame {} / 6562", f);
         let mut frame_path: String = String::from("image_sequence/bad_apple_");
+        
         let file_number = f.to_string();
         for n in file_number.chars() {
             frame_path.push(n);
@@ -27,10 +30,12 @@ fn preload_images(){
     }
     let image_x_pixels = frames[0].width();
     let image_y_pixels = frames[0].height();
-    bad_apple(4, image_x_pixels, image_y_pixels, frames);
+    bad_apple(8, image_x_pixels, image_y_pixels, frames);
 }
 
 fn bad_apple(downscaling: u32, image_x_pixels: u32, image_y_pixels: u32, frames: Vec<DynamicImage>){
+    let mut start = String::new();
+    let mut ascii_frames: Vec<String> = Vec::new();
     let mut frame: String;
     for f in frames {
         frame = "".to_string();
@@ -48,7 +53,16 @@ fn bad_apple(downscaling: u32, image_x_pixels: u32, image_y_pixels: u32, frames:
                 frame += "\n";
             }
         }
-        println!("{frame}");
-        thread::sleep(Duration::from_millis(5));
+        frame += "\n";
+        ascii_frames.push(frame);
+    }
+
+    println!("Press enter to start...");
+    io::stdin().read_line(&mut start).unwrap();
+
+    for af in ascii_frames{
+        print!("{af}");
+        thread::sleep(Duration::from_millis(26));
+        print!("\x1B[2J\x1B[1;1H");
     }
 }
