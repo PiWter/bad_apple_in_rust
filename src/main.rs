76e5af945991d2e3f2314a4e3a858febaf1c2699
PiWter <alfrevr05elpro@gmail.com>
2025-01-07 21:48:10 +0100

    optimization

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ use image::*;
 use std::io;
 
 fn main() {
-    preload_images();
+    let frames = preload_frames();
+
+    let mut start = String::new();
+
+    println!("Press enter to start...");
+    io::stdin().read_line(&mut start).unwrap();
+
+    for af in frames{
+        print!("{af}");
+        thread::sleep(Duration::from_millis(26));
+        print!("\x1B[2J\x1B[1;1H");
+    }
+
 }
 
-/*struct Frames {
-    frame: Vec<String>,
-    image_x_pixels: u32,
-    image_y_pixels: u32
-}*/
-
-fn preload_images(){
-    let mut frames: Vec<DynamicImage> = Vec::new();
+fn preload_frames() -> Vec<String>{
+    let mut ascii_frames: Vec<String> = Vec::new();
 
     for f in 1..=6562{
         println!("Loading frame {} / 6562", f);
@@ -26,44 +32,30 @@ fn preload_images(){
         }
         frame_path += ".png";
 
-        frames.push(image::open(&frame_path).unwrap());
+        ascii_frames.push(image_to_ascii(&frame_path, 4));
     }
-    let image_x_pixels = frames[0].width();
-    let image_y_pixels = frames[0].height();
-    bad_apple(8, image_x_pixels, image_y_pixels, frames);
+    ascii_frames
 }
 
-fn bad_apple(downscaling: u32, image_x_pixels: u32, image_y_pixels: u32, frames: Vec<DynamicImage>){
-    let mut start = String::new();
-    let mut ascii_frames: Vec<String> = Vec::new();
-    let mut frame: String;
-    println!("Just a bit more :)");
-    for f in frames {
-        frame = "".to_string();
-        for y in 1..image_y_pixels {
-            for x in 1..image_x_pixels {
-                if y % (downscaling * 2) == 0 && x % (downscaling * 2) == 0{
-                    if f.get_pixel(x, y)[0] <= 200 && f.get_pixel(x, y)[1] <= 200 && f.get_pixel(x, y)[2] <= 200{
-                        frame += "  ";
-                    } else {
-                        frame += "+ ";
-                    }
+fn image_to_ascii(frame_path: &String, downscaling: u32) -> String{
+    let img_frame = image::open(&frame_path).unwrap();
+    let image_y_pixels = img_frame.height();
+    let image_x_pixels = img_frame.width();
+    let mut ascii_frame = "".to_string();
+    for y in 1..image_y_pixels {
+        for x in 1..image_x_pixels {
+            if y % (downscaling * 2) == 0 && x % (downscaling * 2) == 0{
+                if img_frame.get_pixel(x, y)[0] <= 200 && img_frame.get_pixel(x, y)[1] <= 200 && img_frame.get_pixel(x, y)[2] <= 200{
+                    ascii_frame += "  ";
+                } else {
+                    ascii_frame += "+ ";
                 }
             }
-            if y % (downscaling * 2) == 0{
-                frame += "\n";
-            }
         }
-        frame += "\n";
-        ascii_frames.push(frame);
+        if y % (downscaling * 2) == 0{
+            ascii_frame += "\n";
+        }
     }
-
-    println!("Press enter to start...");
-    io::stdin().read_line(&mut start).unwrap();
-
-    for af in ascii_frames{
-        print!("{af}");
-        thread::sleep(Duration::from_millis(26));
-        print!("\x1B[2J\x1B[1;1H");
-    }
+    ascii_frame += "\n";
+    ascii_frame
 }
